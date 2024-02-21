61016e89a51a414588e598d615baec8f3fea7d0c
Reales Myles <rclarence123@gmail.com>
2024-02-21 18:20:20 -0500

    Initial Commit

---
+++ Diseno/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Diseno
+//
+//  Created by Reales Rectoro Myles Clarence on 21/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Diseno/DisenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisenoApp.swift
+//  Diseno
+//
+//  Created by Reales Rectoro Myles Clarence on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DisenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
