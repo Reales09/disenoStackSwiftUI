9c75cc72afaac1c83824215a6bf81a6cfe2454a9
Reales Myles <rclarence123@gmail.com>
2024-02-25 23:48:32 -0500

    Size Classes

--- Diseno/ContentView.swift
+++ Diseno/ContentView.swift
@@ -12,25 +12,68 @@ import SwiftUI
 // zstack fondo
 
 struct ContentView: View {
+    // Valores de entorno o enviromenteValues
+    @Environment(\.horizontalSizeClass) var sizeClass
+    
+    
     var body: some View {
+        if sizeClass == .regular{
+            regularDesign()
+
+        }else{
+            compactDesign()
+
+        }
+    }
+}
+
+struct compactDesign: View {
+    var body: some View{
         ZStack{
             Color.blue.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            HStack(){
-                Image("car")
-                    .resizable()
-                    .frame(width: 130, height: 100, alignment: .center)
+            VStack(){
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Container@*/VStack/*@END_MENU_TOKEN@*/ {
+                    Image("car")
+                          .resizable()
+                      .frame(width: 130, height: 100, alignment: .center)
+                      .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+                Text("Maria Ramirez")
+                    .font(.largeTitle)         .foregroundColor(.white)
+                    .bold()
+                Text("Calle 123")
+                    .foregroundColor(.white)
+                    .font(.title).italic()
                 
-                VStack(alignment: .leading, spacing: 10){
-                    Text("Maria Ramirez").font(.largeTitle).foregroundColor(.white)
-                        .bold()
+                
+            }
+        }
+    }
+}
+
+struct regularDesign: View {
+    var body: some View{
+        ZStack{
+            Color.green.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            HStack(){
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Container@*/VStack/*@END_MENU_TOKEN@*/ {
+                    Image("car")
+                          .resizable()
+                      .frame(width: 130, height: 100, alignment: .center)
+                      .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+                
+                VStack(alignment: .trailing, spacing: 10){
+                    Text("Maria Ramirez")
+                           .font(.largeTitle)         .foregroundColor(.white)
+                           .bold()
                     Text("Calle 123")
-                        .foregroundColor(.white).font(.title).italic()
+                           .foregroundColor(.white)
+                         .font(.title).italic()
                     
                 }
             }
         }
-
-        
     }
 }
 
